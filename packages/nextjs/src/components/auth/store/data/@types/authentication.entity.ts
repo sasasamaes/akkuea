42c5e13b5fc6d5894206ffa6f47b86b67a1ad41b
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-02-07 18:55:52 -0600

    feat: introduces a global authentication for the application

---
+++ packages/nextjs/src/components/auth/store/data/@types/authentication.entity.ts
@@ -0,0 +1,8 @@
+export interface AuthenticationStore {
+    address: string;
+    name: string;
+  
+    connectWalletStore: (address: string, name: string) => void;
+    disconnectWalletStore: () => void;
+  }
+  
