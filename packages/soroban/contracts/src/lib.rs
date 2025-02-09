88cb0449a16460bfe2ca3473171912a1744ecd21
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-02-08 21:12:23 -0600

    change: some changes in the hello world contract

--- packages/soroban/contracts/src/lib.rs
+++ packages/soroban/contracts/src/lib.rs
@@ -1,13 +1,12 @@
-#![no_std]
-use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
+use soroban_sdk::{contract, contractimpl, vec, symbol_short, BytesN, Env, Symbol, Vec};
 
 #[contract]
 pub struct Contract;
 
 #[contractimpl]
 impl Contract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
+    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
+        vec![&env, symbol_short!("Hello"), to]
     }
 }
 
