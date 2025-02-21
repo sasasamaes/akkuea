9685754d1ec16995c85d54398b3f116bd9bfc9a9
Josue19-08 <josuemarin2009@hotmail.com>
2025-02-20 23:01:56 -0600

    fix: add quickPost into homepage

--- packages/nextjs/src/app/page.tsx
+++ packages/nextjs/src/app/page.tsx
@@ -1,40 +1,35 @@
-'use client'
+"use client"
 
-import { useWallet } from "@/components/auth/hooks/useWallet.hook";
-import { useGlobalAuthenticationStore } from "@/components/auth/store/data";
-import { useState } from "react";
+import { useWallet } from "@/components/auth/hooks/useWallet.hook"
+import { useGlobalAuthenticationStore } from "@/components/auth/store/data"
 import { Button } from "@/components/ui/button"
+import QuickPost from "@/components/quickPost/quickPost"
 
 export default function Home() {
-  const { handleConnect, handleDisconnect } = useWallet();
-  const address = useGlobalAuthenticationStore((state) => state.address);
+  const { handleConnect, handleDisconnect } = useWallet()
+  const address = useGlobalAuthenticationStore((state) => state.address)
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="row-start-1">
+    <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] w-full">
+      <header className="w-full flex justify-center">
         {address ? (
-            <Button onClick={handleDisconnect}>
-              Disconnect
-            </Button>
-          ) : (
-            <Button onClick={handleConnect}>
-              Connect
-            </Button>
-          )}
+          <Button onClick={handleDisconnect}>Disconnect</Button>
+        ) : (
+          <Button onClick={handleConnect}>Connect</Button>
+        )}
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 w-full items-center">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <h1>Hi</h1>
           <p>{address}</p>
         </div>
+        <QuickPost />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        >
+      <footer className="w-full flex gap-6 flex-wrap items-center justify-center">
+        <a className="flex items-center gap-2 hover:underline hover:underline-offset-4">
           Footer
         </a>
       </footer>
     </div>
-  );
+  )
 }
