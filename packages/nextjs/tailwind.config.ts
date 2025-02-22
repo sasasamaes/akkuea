fe4b17f474ded0b6f461572a7906f2b1b5345eb3
Francisco Campos Díaz <sasasamaes@users.noreply.github.com>
2025-02-21 19:19:53 -0600

    add private messages pages

--- packages/nextjs/tailwind.config.ts
+++ packages/nextjs/tailwind.config.ts
@@ -6,8 +6,17 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/**/*.{ts,tsx}",
+    "*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
+  theme: { 
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
