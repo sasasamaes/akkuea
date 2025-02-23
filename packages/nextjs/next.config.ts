337897cadb4541c41b0fb58bc35454b34ffdcc05
Francisco Campos Díaz <sasasamaes@users.noreply.github.com>
2025-02-23 08:42:04 -0600

    fix

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["avatars.githubusercontent.com", "via.placeholder.com","react.semantic-ui.com"],
+    domains: ['avatars.githubusercontent.com', 'via.placeholder.com', 'react.semantic-ui.com'],
   },
 };
 
